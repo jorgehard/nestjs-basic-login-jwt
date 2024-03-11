ee8949f148d516ad703180fa965f67ba3d23d8c2
Jorge H Baptista <jorgehenrique@live.com>
2024-03-10 23:58:41 -0300

    finish module 4

---
+++ src/user/user.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor() {}
+
+  @Post()
+  async create(@Body() body: CreateUserDTO) {
+    return { body };
+  }
+
+  @Get()
+  async listAll() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async listOne(@Param('id', ParseIntPipe) id) {
+    return { user: [id] };
+  }
+
+  @Put(':id')
+  async update(@Body() body: UpdatePutUserDTO, @Param('id', ParseIntPipe) id) {
+    return { body, id };
+  }
+
+  @Patch(':id')
+  async updatePatch(@Body() body: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id) {
+    return { body,id };
+  }
+
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id) {
+    return { id };
+  }
+}
